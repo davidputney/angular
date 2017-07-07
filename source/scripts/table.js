0695607c46ba113928feece1ebed45aed3827a73
davidputney <david.putney@cantina.co>
2017-07-07 14:32:01 -0400

    Adds code that downloads notes from Firebase and renders them. Last working version.

--- source/scripts/table.js
+++ source/scripts/table.js
@@ -1,29 +1,98 @@
-let headers = ["Book", "Author", "Language", "Published", "Sales"];
-let data = [
-  ["The Lord of the Rings", "J. R. R. Tolkien", "English", "1954-1955", "150 million"],
-  ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupéry", "French", "1943", "140 million"],
-  ["Harry Potter and the Philosopher's Stone", "J. K. Rowling", "English", "1997", "107 million"],
-  ["And Then There Were None", "Agatha Christie", "English", "1939", "100 million"],
-  ["Dream of the Red Chamber", "Cao Xueqin", "Chinese", "1754-1791", "100 million"],
-  ["The Hobbit", "J. R. R. Tolkien", "English", "1937", "100 million"],
-  ["She: A History of Adventure", "H. Rider Haggard", "English", "1887", "100 million"],
-];
-
-
-angular.module('tableApp', [])
-  .controller('tableController', [function() {
-    var table=this;
-
-    table.booksData = data;
-    table.headerData = headers;
-
-    table.testFoo = function(e) {
-        let foo = table.headerData.indexOf(e.target.innerText);
-        let sorted = table.booksData.sort(function(a, b){
-          if(a[foo] < b[foo]) return -1;
-          if(a[foo] > b[foo]) return 1;
-          return 0;
-        })
-        table.booksData = sorted;
+angular.module('notesApp', ['ngRoute'])
+.value('Constant', {
+  MAGIC_NUMBER: 42
+})
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+      templateUrl: '/views/notes.html',
+      controller: 'notesController as notes'
+    });
+  }])
+  .controller('notesController', ['getData', function(getData) {
+    var self=this;
+    let data;
+    let dataUrl = 'https://putneyangular.firebaseio.com/notes.json'
+    getData.notes(dataUrl).then(function(response) {
+      self.data = response.data;
+    }, function(errResponse) {
     }
-  }]);
+);
+}])
+.factory('getData', ['$http', function($http){
+  return {
+    notes: function(url) {
+      return $http.get(url)
+    }
+  }
+}])
+
+
+
+
+
+// let fooPromise = new Promise(function(resolve, reject) {
+//   var request = new XMLHttpRequest();
+//   var url = 'https://putneyangular.firebaseio.com/users.json'
+//   request.onreadystatechange = function() {
+//     if (request.readyState === 4) {
+//       if (request.status === 200) {
+//         resolve(request.responseText)
+//       } else {
+//         reject('shit')
+//       }
+//     }
+//   };
+//   request.open("GET", url , true);
+//   request.send(null);
+// }).then(function(val) {
+//   console.log('val', val);
+// })
+
+
+// var request = new XMLHttpRequest();
+// var url = 'https://putneyangular.firebaseio.com/users.json'
+// request.onreadystatechange = function() {
+//     if (request.readyState === 4) {
+//         if (request.status === 200) {
+//             document.body.className = 'ok';
+//             console.log(request.responseText);
+//         } else {
+//             document.body.className = 'error';
+//         }
+//     }
+// };
+// request.open("GET", url , true);
+// request.send(null);
+
+
+
+
+//   var notes = [
+//      {
+//        "note": "This is note number one here here hehre",
+//        "author": "Heywood Jablowme",
+//        "done": true
+//      },
+//      {
+//        "note": "This is two this is note text here",
+//        "author": "Seymour Butz",
+//        "done": false
+//      },
+//      {
+//        "note": "This is note three here here here here",
+//        "author": "Cora Spondent",
+//        "done": false
+//      }
+//  ];
+//
+//
+// var database = firebase.database();
+//
+// firebase.database().ref('notes/').set(notes);
+
+// let foo = firebase.database().ref('/users/');
+//
+// foo.on('value', function(snapshot) {
+//   console.log('val', snapshot.val().chats);
+//
+// })
